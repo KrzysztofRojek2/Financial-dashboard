a43de15533954bd435d9353eb66cc42d434c99fe
KrzysztofRojek <krzysztof-rojek7@wp.pl>
2025-03-29 13:08:35 +0100

    Update FooterSection.tsx

--- Frontend/src/components/FooterSection.tsx
+++ Frontend/src/components/FooterSection.tsx
@@ -7,8 +7,10 @@ const FooterSection: React.FC<FooterSectionProps> = ({ header, points }) => {
   return (
     <>
       <h2 className="font-medium pb-2">{header}</h2>
-      {points.map((point, _index) => (
-        <p className="text-sm cursor-pointer hover:underline">{point}</p>
+      {points.map((point, index) => (
+        <p key={index} className="text-sm cursor-pointer hover:underline">
+          {point}
+        </p>
       ))}
     </>
   );
